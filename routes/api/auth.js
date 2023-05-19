9b64da6997b82a9c0ebc89a1641f0ba42ea2e7f1
Omid Noorshams <o.noorshams@gmail.com>
2023-05-18 17:17:33 -0700

    find user by id in auth.js. text by postman and save

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 
 const router = express.Router();
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get("/", (req, res) => {
-  res.send("Auth router works");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
